9e06d26f19d586d1f41382818d4a134a5bc8cf59
Richard Widén <richardwiden@gmail.com>
2016-10-02 20:33:13 +0200

    7. HTTP HerosearchComponent - INITIALIZE THE HEROES PROPERTY (NGONINIT

---
+++ tou/app/hero-search.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {Hero} from "./hero";
+import {Observable} from "rxjs";
+@Injectable()
+export class HeroSearchService {
+    private searchUrl = 'app/heroes';
+
+    constructor(private http: Http) {
+    }
+
+    search(term: String): Observable<Hero[]> {
+        return this.http.get(`${this.searchUrl}/?name=${term}`)
+            .map((r: Response)=> r.json().data as Hero []);
+    }
+}
