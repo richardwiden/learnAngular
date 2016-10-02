e176e159cd6daf16d7861c734e56c2ff95ee8bdf
Richard Widén <richardwiden@gmail.com>
2016-10-02 19:50:13 +0200

    7. HTTP HTTP Promise

--- tou/app/app.module.ts
+++ tou/app/app.module.ts
@@ -7,10 +7,15 @@ import {HeroService} from "./hero.service";
 import {HeroesComponent} from "./heroes.component";
 import {routing} from "./app.routing";
 import {DashboardComponent} from "./dashboard.component";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         routing],
     declarations: [
         AppComponent,
--- tou/app/hero.service.ts
+++ tou/app/hero.service.ts
@@ -1,13 +1,26 @@
 import {Injectable} from "@angular/core";
 import {Hero} from "./hero";
-import {HEROES} from './mock-heroes'
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'app/heroes';
+
+    constructor(private http: Http) {
+    }
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl).toPromise()
+            .then(response => response.json().data as Hero [])
+            .catch(this.handleError);
     }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error ocurred', error);
+        return Promise.reject(error.message() || error);
     }
 }
